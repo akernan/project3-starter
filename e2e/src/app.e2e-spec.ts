2885e3712396503b9a443cca1e6581800305fac2
akernan2 <akernan2@cs6261-vm03.uwg.westga.edu>
2021-07-07 03:23:58 -0400

    implemented registration error tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -92,7 +92,58 @@ it('AUTO GEN 2 PLAYERS, Player Leia WINS', () => {
   });
 
 describe('Registration Error Tests', () => {
-  browser.get('/registration');
+
+  it('NO PLAYERS, SHOWS MESSAGE', () => {
+    browser.get('/');
+    element.all(by.partialLinkText('Registration')).click();
+    element(by.id('submit')).click();
+    expect(element(by.id('message')).getText()).toBe('Should be 2, 4, or 8 contestants');
+  });
+
+  it('ONE PLAYER, SHOWS MESSAGE', () => {
+    browser.get('/');
+    element.all(by.partialLinkText('Registration')).click();
+    element(by.id('contestant0')).sendKeys('Parzival');
+    element(by.id('submit')).click();
+    expect(element(by.id('message')).getText()).toBe('Should be 2, 4, or 8 contestants');
+  });
+
+  it('THREE PLAYERS, SHOWS MESSAGE', () => {
+    browser.get('/');
+    element.all(by.partialLinkText('Registration')).click();
+    element(by.id('contestant0')).sendKeys('Parzival');
+    element(by.id('contestant1')).sendKeys('Art3mis');
+    element(by.id('contestant2')).sendKeys('TGAP_Ogg');
+    element(by.id('submit')).click();
+    expect(element(by.id('message')).getText()).toBe('Should be 2, 4, or 8 contestants');
+  });
+
+  it('FIVE PLAYERS, SHOWS MESSAGE', () => {
+    browser.get('/');
+    element.all(by.partialLinkText('Registration')).click();
+    element(by.id('contestant0')).sendKeys('Parzival');
+    element(by.id('contestant1')).sendKeys('Art3mis');
+    element(by.id('contestant2')).sendKeys('TGAP_Ogg');
+    element(by.id('contestant3')).sendKeys('Aech');
+    element(by.id('contestant4')).sendKeys('Sho');
+    element(by.id('submit')).click();
+    expect(element(by.id('message')).getText()).toBe('Should be 2, 4, or 8 contestants');
+  });
+
+  it('Seven PLAYERS, SHOWS MESSAGE', () => {
+    browser.get('/');
+    element.all(by.partialLinkText('Registration')).click();
+    element(by.id('contestant0')).sendKeys('Parzival');
+    element(by.id('contestant1')).sendKeys('Art3mis');
+    element(by.id('contestant2')).sendKeys('TGAP_Ogg');
+    element(by.id('contestant3')).sendKeys('Aech');
+    element(by.id('contestant4')).sendKeys('Sho');
+    element(by.id('contestant5')).sendKeys('Anorak');
+    element(by.id('contestant6')).sendKeys('Daito ');
+    element(by.id('submit')).click();
+    expect(element(by.id('message')).getText()).toBe('Should be 2, 4, or 8 contestants');
+  });
+
 });
 
 describe('Bracket Error Tests', () => {
